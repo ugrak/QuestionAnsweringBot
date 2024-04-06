783ad19db49cda2df6ea4e5639bbacc476e4758a
ugrak <augrak@yandex.ru>
2024-04-06 18:07:42 +0300

    First stage

---
+++ solution.py
@@ -0,0 +1,4 @@
+print("Hello! I'm (the QA bot name you chose), a question answering bot "
+      "who knows answers to all questions from the 'Jeopardy!' game.")
+input("Ask me something!")
+print("Let's play!")
